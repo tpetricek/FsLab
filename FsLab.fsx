a091b5c5839432cdaca79235ffba672aa76f4a6f
Gustavo Guerra <gustavo@codebeside.org>
2013-10-23 12:00:26 +0100

    Update FsLab.fsx

--- FsLab.fsx
+++ FsLab.fsx
@@ -1,35 +1,29 @@
-#r "System.Windows.Forms.DataVisualization.dll"
 #nowarn "211"
-#I "../bin"
+#I "packages/FSharp.Charting.0.87/lib/net40"
+#I "packages/FSharp.Data.1.1.10/lib/net40"
+#I "packages/Deedle.0.9.4-beta/lib/net40"
+#I "packages/MathNet.Numerics.2.6.2/lib/net40"
+#I "packages/MathNet.Numerics.FSharp.2.6.0/lib/net40"
+#I "../packages/FSharp.Charting.0.87/lib/net40"
+#I "../packages/FSharp.Data.1.1.10/lib/net40"
+#I "../packages/Deedle.0.9.4-beta/lib/net40"
+#I "../packages/MathNet.Numerics.2.6.2/lib/net40"
+#I "../packages/MathNet.Numerics.FSharp.2.6.0/lib/net40"
 #I "../../packages/FSharp.Charting.0.87/lib/net40"
 #I "../../packages/FSharp.Data.1.1.10/lib/net40"
 #I "../../packages/Deedle.0.9.4-beta/lib/net40"
-#I "../../packages/RProvider.1.0.3/lib"
+#I "../../packages/MathNet.Numerics.2.6.2/lib/net40"
+#I "../../packages/MathNet.Numerics.FSharp.2.6.0/lib/net40"
 #I "../../../packages/FSharp.Charting.0.87/lib/net40"
 #I "../../../packages/FSharp.Data.1.1.10/lib/net40"
 #I "../../../packages/Deedle.0.9.4-beta/lib/net40"
-#I "../../../packages/RProvider.1.0.3/lib"
+#I "../../../packages/MathNet.Numerics.2.6.2/lib/net40"
+#I "../../../packages/MathNet.Numerics.FSharp.2.6.0/lib/net40"
+#r "System.Windows.Forms.DataVisualization.dll"
 #r "FSharp.Charting.dll"
 #r "FSharp.Data.dll"
 #r "Deedle.dll"
-#r "RProvider.dll"
-#r "RDotNet.dll"
-
-namespace FsLab
-open FSharp.Charting
-
-module FsiAutoShow = 
-  fsi.AddPrinter(fun (ch:FSharp.Charting.ChartTypes.GenericChart) -> 
-    ch.ShowChart(); "(Chart)")
-
-namespace FSharp.Charting
-open FSharp.Charting
-open Deedle
-
-[<AutoOpen>]
-module FsLabExtensions =
-  type FSharp.Charting.Chart with
-    static member Line(data:Series<'K, 'V>, ?Name, ?Title, ?Labels, ?Color, ?XTitle, ?YTitle) =
-      Chart.Line(Series.observations data, ?Name=Name, ?Title=Title, ?Labels=Labels, ?Color=Color, ?XTitle=XTitle, ?YTitle=YTitle)
-    static member Area(data:Series<'K, 'V>, ?Name, ?Title, ?Labels, ?Color, ?XTitle, ?YTitle) =
-      Chart.Area(Series.observations data, ?Name=Name, ?Title=Title, ?Labels=Labels, ?Color=Color, ?XTitle=XTitle, ?YTitle=YTitle)
+#r "MathNet.Numerics.dll"
+#r "MathNet.Numerics.IO.dll"
+#r "MathNet.Numerics.FSharp.dll"
+#load "SetupCharting.fsx"
